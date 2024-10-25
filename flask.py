6ea12ee5a5203b3439d929672347a2dfe2f833a2
matanlevipaor <matanlevipaor@users.noreply.github.com>
2024-10-25 13:34:40 +0000

    deploy: 5068e497fb1dc9edb94354cb4a9492cf26d27bdd

---
+++ flask.py
@@ -0,0 +1,11 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/api/student', methods=['GET'])
+def student():
+    name = request.args.get('name', 'Guest')
+    return jsonify(message=f"Hello, {name}! Welcome to the Flask application.")
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
